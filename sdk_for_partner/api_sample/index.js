db99624d48c65384849e9ac59035e8aec74985dc
Vincent Hsu <rashoi.tw@gmail.com>
2022-01-17 12:52:08 +0800

    app api sample

---
+++ sdk_for_partner/api_sample/index.js
@@ -0,0 +1,174 @@
+const axios = require('axios').default;
+const open = require('open');
+
+console.log('Meshstream API Sample');
+
+
+let ServerUrl ="https://api.meshstream.io"
+let userAccount ="test.meshstream@gmail.com";
+let userPassword ="123456";  
+let projectId="cd46f66d-b24f-4b0a-8e34-23f805cfc9ff";
+
+
+
+//init Room option
+function initRoomOption ()
+{
+  var ui = new Object();
+  ui.primaryColor="#71c9e9";
+  ui.primaryFontColor="#000000";
+  ui.secondaryColor="#ffffff";
+  ui.secondaryFontColor="#000000";
+  ui.defaultLayout="auto";
+  ui.notificationPosition="rightTop";
+  ui.enableChatroom=true;
+  ui.enableWhiteboard=true;
+  ui.enableRecording=true;
+  ui.enableFileSharing=true;
+  ui.enableScreenSharing=true;
+  ui.allowAttendeesUnmuteThemselves=true;
+  ui.openCameraOnEntry=false;
+  ui.openMicOnEntry=true;
+
+  var youtubeSetting= new Object();
+  youtubeSetting.pushYoutube=false;
+  youtubeSetting.ytParams= {
+    publish_url: "rtmp://a.rtmp.youtube.com/live2",
+    view_url: "https://youtu.be/ADuGn9lOTqQ",
+    stream_key: "5c0e-5pey-rytu-rfj4-8asx"
+  };
+  var roomOption = new Object();
+  roomOption.ui = ui;
+  roomOption.youtubeSetting=youtubeSetting;
+
+  return roomOption;
+
+}
+
+
+
+async function Login(account,password){
+  var token =""
+  await axios
+  .post(ServerUrl+'/v2/login', {
+    account: account,
+    password:password
+  })
+  .then(res => {
+    token = res.data["token"]
+  })
+  .catch(error => {
+    console.error(error)
+  })
+  return token
+}
+
+
+
+async function CreateProject(token,ProjectName,staffs,roomOption){
+  var roomId ="";
+  const auth = {headers: {Authorization:'Bearer ' + token} }
+  await axios
+  .post(ServerUrl+'/v2/project/', 
+    { project:projectId,
+      staff:staffs,
+      name:ProjectName,
+        option:roomOption
+      },
+  auth)
+  .then(res => {
+    ProjectId = res.data["projectItem"]["projectId"]
+  })
+  .catch(error => {
+    console.error(error)
+  })
+
+  return ProjectId
+}
+
+
+
+async function CreateRoom(token,projectId,roomName,roomOption,duration){
+  var roomId ="";
+  const auth = {headers: {Authorization:'Bearer ' + token} }
+  await axios
+  .post(ServerUrl+'/v2/project/rooms', 
+    { projectId:projectId,
+      room:{ 
+        name:roomName,
+        option:roomOption,
+        duration:duration
+      }},
+  auth)
+  .then(res => {
+    roomId = res.data["roomItem"]["roomId"]
+  })
+  .catch(error => {
+    console.error(error)
+  })
+
+  return roomId
+}
+
+async function GetRoomUrl(token,roomId){
+  var url=""
+  const auth = {headers: {Authorization:'Bearer ' + token} }
+  console.log(roomId)
+  await axios
+ .get(ServerUrl+'/v2/project/room/url/'+roomId+"?NeedAuth=true",
+  auth)
+  .then(res => {
+    url =res.data["url"]
+  })
+  .catch(error => {
+    console.error(error)
+    //TODO err create project
+  })
+  return url
+}
+
+
+
+async function Exec (){
+
+  // Step 1: Login ,須先取得測試帳號
+  var token = await Login(userAccount,userPassword)
+
+  // Step 2: Create Room , 須先取得Project ID
+  var roomOption = initRoomOption();
+  let roomName = "Meeting Room For SDK";
+  let begin_time="2022-01-14T03:51:14.676707Z";
+  let end_time="2022-01-14T03:51:14.676709Z";
+
+  //iStaging 客製化示範
+  //roomOption.ui.logoUrl="https://meshub.io/static/media/logo.293c4ef3.webp";
+  //roomOption.ui.primaryColor="#ff0000";
+
+  //youtubeOption 示範 
+  roomOption.youtubeSetting.pushYoutube=true;
+  roomOption.youtubeSetting.ytParams= {
+    publish_url: "rtmp://a.rtmp.youtube.com/live2",
+    view_url: "https://youtu.be/ADuGn9lOTqQ",
+    stream_key: "5c0e-5pey-rytu-rfj4-8asx"
+  };
+
+  var duration= new Object();
+  duration.begin_time=begin_time;
+  duration.end_time=end_time;
+  
+  var roomId = await CreateRoom(token,projectId,roomName,roomOption,duration)
+
+  // Step 3: GetRoomUrl，實際流程為客戶端啟動，在這邊實作是為了整體流程展現。
+  var url = await GetRoomUrl(token,roomId)
+
+  // Step 4: Launch Room with default Browser，實際流程為客戶端啟動，在這邊實作是為了整體流程展現。
+  open(url)
+
+  // Step 5 : Launch Ｒoom with Secondary Browser，實際流程為客戶端啟動，在這邊實作是為了整體流程展現。
+  var roomUrl = "https://conference.meshstream.io/"+roomId;
+  open(roomUrl, {app: {name: 'safari'}});
+
+} 
+
+Exec()
+
