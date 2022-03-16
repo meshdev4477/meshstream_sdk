d2dbe34e1e98b42005a609d1e99bf70fb6bc4c7f
kenchen <independentchen@gmail.com>
2022-03-16 15:01:12 +0800

    Fix: new demo page

--- demo/main.js
+++ demo/main.js
@@ -6,16 +6,77 @@ const $ = document.querySelector.bind(document);
 
 let streamingSdk;
 
-const serverUrl = "wss://meshstream-server.meshstream.io";
+const serverUrl = "https://meshstream-server-back.meshstream.io";
 
 main();
 
 function main(){
   streamingSdk = new StreamingSdk({ peerId, serverUrl });
 
-  streamingSdk.on('streamEnded', async ({ streamName }) => {
-    removeRemoteMedia(streamName);
-    await streamingSdk.unsubscribe({ streamName })
+  streamingSdk
+    .on('joinEvent:error', ({ error }) => {
+      alert(error);
+    })
+    .on('joinInfo', async (joinInfo) => {
+      console.log(joinInfo);
+      alert("Join event successfully");
+      $('#btn-joinEvent').disabled = true;
+      $('#btn-checkStatusByStreamName').disabled = false;
+      $('#btn-publish').disabled = false;
+      $('#btn-subscribe').disabled = false;
+    })
+    .on('publishingStream', async ({ streamName }) => {
+      console.log("A stream is published", streamName);
+    })
+    .on('streamEnded', async ({ streamName }) => {
+      removeRemoteMedia(streamName);
+      await streamingSdk.unsubscribe({ streamName })
+    })
+
+  $('#btn-createEvent').addEventListener('click', async () => {
+    const title = $('#input-title').value.trim();
+    const description = $('#input-description').value.trim();
+    if(!title || !description) {
+      alert("title and description shouldn't be empty");
+      return;
+    }
+
+    const startedAt = new Date(); // You can also define a time later than the current one
+
+    const { success, message, eventId } = await streamingSdk.createEvent({ title, description, startedAt });
+    if(!success) {
+      alert(`create event error: ${message}`);
+      return;
+    }
+
+    $('#input-title').value = null;
+    $('#input-description').value = null;
+    alert(`create an event successfully: ${eventId}`);
+
+  })
+
+  $('#btn-joinEvent').addEventListener('click', async () => {
+    const eventId = $('#input-eventId').value.trim();
+    if(!eventId) {
+      alert("publish streamName shouldn't be empty");
+      return;
+    }
+    await streamingSdk.joinEvent({ eventId });
+    // If success, the joinInfo will be sent to the eventListener in streamingSdk
+  })
+
+  $('#btn-checkStatusByStreamName').addEventListener('click', async () => {
+    const streamName = $('#input-publish-streamName').value.trim();
+    if(!streamName) {
+      alert("publish streamName shouldn't be empty");
+      return;
+    }
+
+    const { isPublished } = await streamingSdk.checkStatusByStreamName({ streamName });
+
+    isPublished ?
+      alert("The streamName is already published!") :
+      alert("The streamName is not published yet.")
   })
 
   $('#btn-publish').addEventListener('click', async () => {
@@ -24,7 +85,7 @@ function main(){
       alert("publish streamName shouldn't be empty");
       return;
     }
-
+    $('#input-publish-streamName').value = null;
     const mediaStream = await navigator.mediaDevices.getUserMedia({
       video: true,
       audio: true 
@@ -32,7 +93,7 @@ function main(){
 
     const { success, error } = await streamingSdk.publish({ streamName, mediaStream });
     if(!success){
-      alert("Publish error:", error);
+      alert(`Publish error: ${error}`);
       return;
     }
     alert("Publish successfully!");
@@ -40,17 +101,17 @@ function main(){
   })
   
   $('#btn-subscribe').addEventListener('click', async () => {
-
     const streamName = $('#input-subscribe-streamName').value.trim();
     if(!streamName) {
       alert("subscribe streamName shouldn't be empty");
       return;
     }
 
+    $('#input-subscribe-streamName').value = null;
     const { success, error, mediaStream } = await streamingSdk.subscribe({ streamName });
     console.log({ success, error, mediaStream })
     if(!success){
-      alert("subscribe error:", error);
+      alert(`subscribe error: ${error}`);
       return;
     }
 
